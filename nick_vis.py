2c7338744155b0516191988743a1c691bc3db83f
raynbowy23 <lov.us.basketball@gmail.com>
2021-10-26 09:33:04 -0500

    fix the image issue

--- nick_vis.py
+++ nick_vis.py
@@ -110,7 +110,7 @@ def visualize():
 
     # Rendering
     # pio.write_html(fig, file='docs/figure.html', full_html=False, auto_open=True)
-    fig.write_image('images/fig.svg')
+    fig.write_image('./docs/images/fig.svg')
 
 if __name__ == "__main__":
     visualize()
